6fd2f1f4d5f1407355586eb746e2b49b45f3b44c
Aisha kafita <116567179+Aishapop@users.noreply.github.com>
2023-05-07 14:19:59 +0200

    Add files via upload

---
+++ Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EOB
+{
+    internal class Transaction
+    {
+        protected int TransactionId { get; set; }
+        protected Account FromAccount { get; set; }
+        protected Account ToAccount { get; set; }
+        protected int Amount { get; set; }
+        protected DateOnly Date { get; set; }
+
+        public Transaction()
+        {
+
+        }
+
+    }
+}
---
+++ User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EOB
+{
+    internal class User
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        protected string Password { get; set; }
+        protected string Email { get; set; }
+
+
+        public User(string firstname, string lastname, string password, string email)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+            Password = password;
+            Email = email;
+        }
+
+    }
+
+      
+
+}

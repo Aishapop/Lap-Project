31bed91297f25ec3f26aaf1270e73ce19d528d98
alanl <r0934629@student.thomasmore.be>
2023-05-12 10:29:41 +0200

    alan - 12/05: firstpage form

--- EOB/Firstpage.Designer.cs
+++ EOB/Firstpage.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(334, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create Account";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Firstpage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Name = "Firstpage";
             this.Text = "Firstpage";
             this.Load += new System.EventHandler(this.Firstpage_Load);
@@ -43,5 +54,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }

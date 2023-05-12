c34b0b99e86e3da83b503fd7cee0241851495ecc
alanl <r0934629@student.thomasmore.be>
2023-05-12 10:20:07 +0200

    alan 12/05: firstpage form

---
+++ EOB/Firstpage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EOB
+{
+    public partial class Firstpage : Form
+    {
+        public Firstpage()
+        {
+            InitializeComponent();
+        }
+
+        private void Firstpage_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

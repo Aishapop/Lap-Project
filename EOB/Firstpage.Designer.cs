0361a49dbef163e0a073127fa91cd2474b40f7a1
alanl <r0934629@student.thomasmore.be>
2023-05-17 15:41:22 +0200

    Alan 17/05 : firstpage, wordklant page

--- EOB/Firstpage.Designer.cs
+++ EOB/Firstpage.Designer.cs
@@ -28,33 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Firstpage));
+            this.wordKlantButton = new System.Windows.Forms.Button();
+            this.aanmeldButton = new System.Windows.Forms.Button();
+            this.adminButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // wordKlantButton
             // 
-            this.button1.Location = new System.Drawing.Point(334, 236);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(128, 29);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Create Account";
-            this.button1.UseVisualStyleBackColor = true;
+            this.wordKlantButton.Location = new System.Drawing.Point(187, 248);
+            this.wordKlantButton.Name = "wordKlantButton";
+            this.wordKlantButton.Size = new System.Drawing.Size(200, 30);
+            this.wordKlantButton.TabIndex = 0;
+            this.wordKlantButton.Text = "Word Klant";
+            this.wordKlantButton.UseVisualStyleBackColor = true;
+            this.wordKlantButton.Click += new System.EventHandler(this.wordKlantButton_Click);
+            // 
+            // aanmeldButton
+            // 
+            this.aanmeldButton.Location = new System.Drawing.Point(187, 295);
+            this.aanmeldButton.Name = "aanmeldButton";
+            this.aanmeldButton.Size = new System.Drawing.Size(200, 30);
+            this.aanmeldButton.TabIndex = 1;
+            this.aanmeldButton.Text = "Aanmelden";
+            this.aanmeldButton.UseVisualStyleBackColor = true;
+            this.aanmeldButton.Click += new System.EventHandler(this.aanmeldButton_Click);
+            // 
+            // adminButton
+            // 
+            this.adminButton.Location = new System.Drawing.Point(187, 344);
+            this.adminButton.Name = "adminButton";
+            this.adminButton.Size = new System.Drawing.Size(200, 30);
+            this.adminButton.TabIndex = 2;
+            this.adminButton.Text = "Admin";
+            this.adminButton.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(187, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
             // 
             // Firstpage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(616, 438);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.adminButton);
+            this.Controls.Add(this.aanmeldButton);
+            this.Controls.Add(this.wordKlantButton);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Firstpage";
             this.Text = "Firstpage";
             this.Load += new System.EventHandler(this.Firstpage_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button wordKlantButton;
+        private System.Windows.Forms.Button aanmeldButton;
+        private System.Windows.Forms.Button adminButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
